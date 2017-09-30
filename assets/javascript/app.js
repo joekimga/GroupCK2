82f9920e03769aa3a60688136b98485a0896f8a2
Joseph <joekimga@gmail.com>
2017-09-29 21:37:09 -0400

    WIP: changed search bar

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+
+// displayTwitterInfo function re-renders the HTML to display the appropriate content
+function displayTwitterInfo() {
+
+  var twitter = $(this).attr("data-name");
+  var queryURL = ""
+
+
+
+
+
+
+
+
+}
