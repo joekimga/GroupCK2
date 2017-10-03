900aed23f97e39a9d0822d394ee5774348153dec
Joseph <joekimga@gmail.com>
2017-10-03 17:49:34 -0400

    WIP

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,34 @@
 $(document).ready(function(){
 
 // displayeBayInfo function re-renders the HTML to display the appropriate content
+
+/*
 function displayeBayInfo() {
 
   var eBay = $(this).attr("data-name");
-  var queryURL = ""
+  var queryURL = "https://www.ebay.com/"
+*/
 
+  $("button").on("click", function(event) {
+    event.preventDefault();
+    terms = $("#searchTerms").val().trim();
+      var addition = "_costume"
+      var eBayURL = "https://www.ebay.com/" + terms + addition + "";
+        console.log(eBayURL);
+      $.ajax({
+        url: eBayURL
+      }).done(function(response){
+          console.log(response);
+          for (var i = 0; i < 4; i++){
+            var 
+
+          }
+
+
+
+      })
+
+  })
 
 
 
@@ -15,3 +38,5 @@ function displayeBayInfo() {
 
   };
 };
+
+
